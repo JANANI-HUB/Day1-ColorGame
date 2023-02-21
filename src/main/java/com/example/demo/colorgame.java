a29a3c07566e185adc1c18cd19ea5e34ae33e069
JANANI-HUB <727721euit059@skcet.ac.in>
2023-02-21 12:06:42 +0530

    First commit

---
+++ src/main/java/com/example/demo/colorgame.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+
+public class colorgame {
+	@RequestMapping("/print")
+	public String display(HttpServletRequest req)
+	{
+		String color=req.getParameter("color");
+		HttpSession session = req.getSession();
+session.setAttribute("colorName", color);
+		
+		return "NewFile1.jsp";
+	}
+
+}
+
